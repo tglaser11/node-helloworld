460f312877fd13e2f559ff3d85d740be9d920a39
Tom Glaser <tfglaser@gmail.com>
2016-05-01 19:29:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var http = require('http'); 
-var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000; 
-var server = http.createServer(function(req,res){ res.writeHead(200, {'Content-Type':'text/plain'}); 
-res.end('Hello World!!'); }); server.listen(port,ip); 
-console.log('Server running at http://%s:%d',ip,port);
+// Load the http module to create an http server.
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(80);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:80/");
